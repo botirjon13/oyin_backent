fe725d18f5e2a7d49cc84a95b9c561c06f0a7ab3
botirjon13 <tomama-uz@mail.ru>
2026-01-14 12:42:49 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,47 @@
+import os
+import psycopg2
 from fastapi import FastAPI
-from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://postgres:PAROL@localhost:5432/postgres"
-
-engine = create_engine(DATABASE_URL)
+# .env ni yuklash
+load_dotenv()
 
 app = FastAPI()
 
+# PostgreSQL ulanish
+conn = psycopg2.connect(
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT"),
+    database=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD")
+)
+
+# Server ishlayaptimi tekshirish
 @app.get("/")
 def root():
-    return {"status": "OK"}
+    return {
+        "status": "OK",
+        "message": "Backend ishlayapti, PostgreSQL ulangan"
+    }
 
+# Users jadvalidan ma'lumot olish
 @app.get("/users")
 def get_users():
-    with engine.connect() as conn:
-        result = conn.execute(text("SELECT * FROM users"))
-        return [dict(row._mapping) for row in result]
+    with conn.cursor() as cur:
+        cur.execute("""
+            SELECT username, phone, role, created_at
+            FROM users
+            ORDER BY created_at DESC
+        """)
+        rows = cur.fetchall()
+
+    return [
+        {
+            "username": r[0],
+            "phone": r[1],
+            "role": r[2],
+            "created_at": r[3]
+        }
+        for r in rows
+    ]
